8682665feb33d5d17e688ce083d4a793c7128031
Abyssrium <33090241+jLooo@users.noreply.github.com>
2024-04-21 10:52:50 +0800

    Update MGM_ped2.py

--- model/MGM_ped2.py
+++ model/MGM_ped2.py
@@ -241,7 +241,6 @@ class CSA(nn.Module):
 class MGM_ped2(nn.Module):
     def __init__(self, t_length=3, n_channel=3):
         super(MGM_ped2, self).__init__()
-        print('MGM_ped2')
 
         self.decoder = Decoder(t_length, n_channel)
         self.decoder_b = Decoder(t_length, n_channel)
